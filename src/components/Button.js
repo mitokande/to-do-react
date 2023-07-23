f1d0a3a90bf378ea316adb75b332cb9c124d5dee
mitokande <benmithat18@gmail.com>
2023-07-23 15:41:27 +0300

    button prop bgcolor

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function Button({ title, onClick }) {
+export default function Button({ title, bgColor, onClick }) {
     return (
-        <button className='btn'>{title}</button>
+        <button
+            className='btn'
+            style={{ backgroundColor: bgColor }}
+        >{title}</button>
     )
+}
+
+Button.defaultProps = {
+    bgColor: 'skyblue'
 }
