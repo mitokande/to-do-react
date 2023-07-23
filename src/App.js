c866f2c2d87b6fa7e0b6f6abb3dd09746ccdf143
mitokande <benmithat18@gmail.com>
2023-07-23 13:13:11 +0300

    import header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <div>To Do App</div>
+    <div>
+      <Header></Header>
+    </div>
   )
 }
